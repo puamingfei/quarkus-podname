486aa22ae5e2f0cd43e5ed2e1a5c807c3a11be01
CYTan <tcy.cloud@gmail.com>
2021-04-13 14:47:16 +0800

    Final test on pipeline in dev namespace

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Fianl!~ from " + InetAddress.getLocalHost().getHostName();
+        return "Aloha! from " + InetAddress.getLocalHost().getHostName();
     }
 }
