1246ea913e33d9b7394d8bd92b912e5101c1606e
CYTan <tcy.cloud@gmail.com>
2021-04-13 08:48:08 +0800

    takton test

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Halo Github~ from " + InetAddress.getLocalHost().getHostName();
+        return "Holaa~ Github~ from " + InetAddress.getLocalHost().getHostName();
     }
 }
