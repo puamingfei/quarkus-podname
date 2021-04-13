faf8c0101d4bff7d0cb92256673671a55f54ecf7
CYTan <tcy.cloud@gmail.com>
2021-04-13 09:28:19 +0800

    git push test

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "hello~ from " + InetAddress.getLocalHost().getHostName();
+        return "heiya~ from " + InetAddress.getLocalHost().getHostName();
     }
 }
