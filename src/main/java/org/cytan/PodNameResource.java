94f8f7feeae8911111f2dd5887b6d1fcf4b4c91e
CYTan <tcy.cloud@gmail.com>
2021-04-13 14:35:28 +0800

    cleaning up

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Githubaa~ from " + InetAddress.getLocalHost().getHostName();
+        return "Fianl!~ from " + InetAddress.getLocalHost().getHostName();
     }
 }
