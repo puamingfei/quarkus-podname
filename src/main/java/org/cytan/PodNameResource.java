3e186fb6d2233dff0ffd32965c2a3cd8147401b3
CYTan <tcy.cloud@gmail.com>
2021-04-13 10:06:48 +0800

    argocd test

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "gogsLA~ from " + InetAddress.getLocalHost().getHostName();
+        return "Argola~ from " + InetAddress.getLocalHost().getHostName();
     }
 }
