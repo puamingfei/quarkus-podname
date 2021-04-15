b3fda19264786456349148cb196ee7d9b94a3b9c
CYTan <tcy.cloud@gmail.com>
2021-04-15 17:25:35 +0800

    remove trigger: bug

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/podname")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Hello! from " + InetAddress.getLocalHost().getHostName();
+        return "Kill Trigger! from " + InetAddress.getLocalHost().getHostName();
     }
 }
