d4193cebf5a44e597eab99fbe736a8c777925dd4
CYTan <tcy.cloud@gmail.com>
2021-05-29 22:04:35 +0800

    Go demo

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -22,6 +22,6 @@ public class PodNameResource {
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Aloha! from " + InetAddress.getLocalHost().getHostName();
+        return "Go! from " + InetAddress.getLocalHost().getHostName();
     }
 }
